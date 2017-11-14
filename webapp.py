d2b9a16d0b061c074e8efee804422bba2a5d3130
michaelmoschitto <mikemoschitto@gmail.com>
2017-11-14 09:26:23 -0800

    Create webapp.py

---
+++ webapp.py
@@ -0,0 +1,8 @@
+from flask import Flask, request, Markup, render_template, flash, Markup
+import os
+import json
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
