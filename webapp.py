2d215da44e5d15c0e9a4dc0be4ca06aa853d4ab4
Michael Moschitto <michaelmoschitto@gmail.com>
2017-11-16 09:21:57 -0800

    m

--- webapp.py
+++ webapp.py
@@ -8,6 +8,10 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+app.route("/")
+def render_main
+    return render_template('DataByName.html')
+
 
 
 
